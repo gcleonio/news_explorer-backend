16c6e7060b276a3dc0a1946f3cfc9c5fd5185ce1
gleonio <gleonio@gmail.com>
2025-05-01 13:43:27 -0700

    created routes for POST /signup and POST /signin

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const userRouter = require("./users");
+const articleRouter = require("./articles");
+// import loginUser controller
+// import createUser controller
+
+// import middleware validation
+
+const NotFoundError = require("../utils/errors/NotFound");
+
+router.use("/users", userRouter);
+router.use("/articles", articleRouter);
+
+router.post("/signup", // validateCreateUser, // createUser);
+router.post("/signin", // validateLoginUser, // loginUser);
+
+router.use((req, res, next) => {
+  next(new NotFoundError("Route not found"));
+});
+
+module.exports = router;
