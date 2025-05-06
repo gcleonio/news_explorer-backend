0586094060ddd850846f8432df62c770471b0eef
gleonio <gleonio@gmail.com>
2025-05-06 15:46:35 -0700

    installed dependencies for routing, validation, logging, error handling, and database connection

--- app.js
+++ app.js
@@ -1,2 +1,55 @@
-// import centralized error-handling middleware (added last)
-// import requestLogger and errorLogger middleware
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const helmet = require("helmet");
+const { errors } = require("celebrate");
+const errorHandler = require("./middlewares/error-handler");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
+const mainRouter = require("./routes/index");
+const { MONGO_DB_CONNECTION } = require("./utils/config");
+
+const { PORT = 3002 } = process.env;
+require("dotenv").config();
+const app = express();
+app.use(express.json());
+
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});
+
+// Middleware
+app.use(helmet());
+app.use(cors());
+
+// Database connection
+mongoose.set("strictQuery", true);
+mongoose.connect(
+  MONGO_DB_CONNECTION,
+  (r) => {
+    console.log("Connected to MongoDB", r);
+  },
+  (e) => {
+    console.error("Error connecting to MongoDB", e);
+  }
+);
+
+// Logging
+app.use(requestLogger);
+
+// Routes
+app.use(mainRouter);
+
+// Error Logging
+app.use(errorLogger);
+
+// Celebreate Error Handler
+app.use(errors());
+
+// Centralized Error Handler
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
--- utils/config.js
+++ utils/config.js
@@ -1,9 +1,10 @@
 const { JWT_SECRET = "dev-secret", NODE_ENV } = process.env;
 
-// extract CONNECT from environmental variables for connecting db
-// extract MONGO_DB_CONNECTION from process.env and set a default value
+const { MONGO_DB_CONNECTION = "mongodb://localhost:27017/news_explorer_db" } =
+  process.env;
 
 module.exports = {
   JWT_SECRET,
   NODE_ENV,
+  MONGO_DB_CONNECTION,
 };
